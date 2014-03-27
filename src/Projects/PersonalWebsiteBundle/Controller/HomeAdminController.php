d66af87618e48a82f41448f43c831e2d9d207a9a
Javier Mellado <javiermellado@gmail.com>
2014-03-27 20:36:57 +0000

    missing files in commit

--- src/Projects/PersonalWebsiteBundle/Controller/HomeAdminController.php
+++ src/Projects/PersonalWebsiteBundle/Controller/HomeAdminController.php
@@ -3,41 +3,76 @@
 namespace Projects\PersonalWebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Projects\PersonalWebsiteBundle\Entity\users;
+use Projects\PersonalWebsiteBundle\Modals\login;
+
 
 class HomeAdminController extends Controller
 {
-    public function indexAction($name)
+	const REMEMBER_ME_VALUE = 1;
+
+    public function indexAction(Request $request)
     {
-        return $this->render('ProjectsPersonalWebsiteBundle:admin:home/index.html.twig', array('name' => $name));
+		$session = $request->getSession();
+		$entityManager = $this->getDoctrine()->getManager();
+		$repository = $entityManager->getRepository('ProjectsPersonalWebsiteBundle:users');
+		if($request->getMethod() == 'POST'){
+			$name = $request->get('name');
+			$password = md5($request->get('password'));
+			$rememberMe = $request->get('remember-me');
+
+			/** @var $user users */
+			$user = $repository->findOneBy(array('name' => $name,'password' => $password));
+			if ($user){
+				if($rememberMe == self::REMEMBER_ME_VALUE){
+					$login = new login();
+					$login->setPassword($password);
+					$login->setName($name);
+					$session->set('login', $login);
+				}
+				return $this->render('ProjectsPersonalWebsiteBundle:admin:home/loggedin.html.twig', array('name' => $user->getName()));
+			} else {
+				return $this->render('ProjectsPersonalWebsiteBundle:admin:home/login.html.twig', array('name' => 'Error'));
+			}
+		} else if($session->has('login')){
+			$login = $session->get('login');
+			$name = $login->getName();
+			$password = $login->getPassword();
+			/** @var $user users */
+			$user = $repository->findOneBy(array('name' => $name,'password' => $password));
+			if ($user){
+				return $this->render('ProjectsPersonalWebsiteBundle:admin:home/loggedin.html.twig', array('name' => $user->getName()));
+			}
+
+		} else {
+			return $this->render('ProjectsPersonalWebsiteBundle:admin:home/login.html.twig');
+		}
     }
 
-	public function loginAction()
-	{
-		$request = $this->getCurrentRequest();
+	public function logoutAction(Request $request){
 		$session = $request->getSession();
+		$session->clear();
+		return $this->render('ProjectsPersonalWebsiteBundle:admin:home/login.html.twig');
 
-		// get the login error if there is one
-		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-			$error = $request->attributes->get(
-				SecurityContext::AUTHENTICATION_ERROR
-			);
-		} else {
-			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
-		}
-
-		return $this->render(
-			'AcmeSecurityBundle:Security:login.html.twig',
-			array(
-				// last username entered by the user
-				'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-				'error'         => $error,
-			)
-		);
 	}
 
-	public function dumpStringAction()
-	{
-		return $this->render('SimpleProfileBundle:Security:dumpString.html.twig', array());
+	public function cacheAction(){
+		$memcache = new \Memcache();
+		$memcache->connect('localhost', 11211);
+
+		$cacheDriver = new \Doctrine\Common\Cache\MemcacheCache();
+		$cacheDriver->setMemcache($memcache);
+		$key = 'test';
+		if( $user = $cacheDriver->fetch( $key ) ) {
+		} else {
+			$entityManager = $this->getDoctrine()->getManager();
+			$repository = $entityManager->getRepository('ProjectsPersonalWebsiteBundle:users');
+			$user = $repository->findOneBy(array('name' => 'javi'));
+			$cacheDriver->save( $key, $user, 3600 );
+		}
+		return $this->render('ProjectsPersonalWebsiteBundle:admin:home/cache.html.twig', array('user' => $user) );
+
 	}
 }
--- src/Projects/PersonalWebsiteBundle/Entity/users.php
+++ src/Projects/PersonalWebsiteBundle/Entity/users.php
@@ -3,7 +3,6 @@
 namespace Projects\PersonalWebsiteBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * users
@@ -11,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Table()
  * @ORM\Entity
  */
-class users implements UserInterface, \Serializable
+class users
 {
     /**
      * @var integer
@@ -113,90 +112,4 @@ class users implements UserInterface, \Serializable
         return $this->admin;
     }
 
-	/**
-	 * @see \Serializable::serialize()
-	 */
-	public function serialize()
-	{
-		return serialize(array(
-			$this->id,
-		));
-	}
-
-	/**
-	 * @see \Serializable::unserialize()
-	 */
-	public function unserialize($serialized)
-	{
-		list (
-			$this->id,
-			) = unserialize($serialized);
-	}
-
-	/**
-	 * Returns the roles granted to the user.
-	 *
-	 * <code>
-	 * public function getRoles()
-	 * {
-	 *     return array('ROLE_USER');
-	 * }
-	 * </code>
-	 *
-	 * Alternatively, the roles might be stored on a ``roles`` property,
-	 * and populated in any number of different ways when the user object
-	 * is created.
-	 *
-	 * @return Role[] The user roles
-	 */
-	public function getRoles()
-	{
-		// TODO: Implement getRoles() method.
-	}
-
-	/**
-	 * Returns the password used to authenticate the user.
-	 *
-	 * This should be the encoded password. On authentication, a plain-text
-	 * password will be salted, encoded, and then compared to this value.
-	 *
-	 * @return string The password
-	 */
-	public function getPassword()
-	{
-		// TODO: Implement getPassword() method.
-	}
-
-	/**
-	 * Returns the salt that was originally used to encode the password.
-	 *
-	 * This can return null if the password was not encoded using a salt.
-	 *
-	 * @return string|null The salt
-	 */
-	public function getSalt()
-	{
-		// TODO: Implement getSalt() method.
-	}
-
-	/**
-	 * Returns the username used to authenticate the user.
-	 *
-	 * @return string The username
-	 */
-	public function getUsername()
-	{
-		// TODO: Implement getUsername() method.
-	}
-
-	/**
-	 * Removes sensitive data from the user.
-	 *
-	 * This is important if, at any given point, sensitive information like
-	 * the plain-text password is stored on this object.
-	 */
-	public function eraseCredentials()
-	{
-		// TODO: Implement eraseCredentials() method.
-	}
 }
